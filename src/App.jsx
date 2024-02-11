4aef594c8fbbdd096746e838a622eb0637dd0d63
Nicolas Mitestainer <nicolas.mitestainer08@gmail.com>
2024-02-11 14:04:54 -0300

    alterado import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Login from "./Pages/Login/Login";
+import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import { UserStorage } from "./contexts/UserContext";
 import User from "./pages/User/User";
@@ -19,21 +19,21 @@ const App = () => {
         <UserStorage>
           <Header />
           <main className="AppBody">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
-            <Route
-              path="conta/*"
-              element={
-                <ProtectedRoute>
-                  <User />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/foto/:id" element={<Photo />} />
-            <Route path="/perfil/:user" element={<UserProfile />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login/*" element={<Login />} />
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/foto/:id" element={<Photo />} />
+              <Route path="/perfil/:user" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
           </main>
           <Footer />
         </UserStorage>
